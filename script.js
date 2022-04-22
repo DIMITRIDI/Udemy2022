89cbd5cee5fda4e2a20abb287454b371b1039af3
Dmitry <d.a16@yandex.ru>
2022-04-22 12:36:11 +0300

    Coding Exercise 3: Loop and Conditional Challenges

--- script.js
+++ script.js
@@ -1,39 +1,41 @@
 "use strict"; 
 
-for (let i = 0; i < 3; i++) {
+// Место для первой задачи
+for (let i = 5; i < 11; i++) {
    console.log(i);
-   for (let j = 0; j < 3; j++) {
-   console.log(j);
+}
+
+// Место для второй задачи
+for (let i = 20; i >= 10; i--) {
+   if (i === 13) {
+      break;
+   }
+   console.log(i);
+}
+
+// Место для третьей задачи
+for (let i = 2; i <= 10; i++) {
+   if (i % 2 === 0) {
+      console.log(i);
    }
 }
 
-// *
-// **
-// ***
-// ****
-// *****
-// ******
+// Место для четвертой задачи
+let i = 3; 
 
-let result = '';
-const leng = 7;
-
-for (let i = 1; i < leng; i++) {
-   for (let j = 0; j < i; j++) {
-      result += "*";
+while (i <= 16) { 
+   if (i % 2 === 0) {
+   } else {
+      console.log(i);
    }
-   result += '\n';
-}
-console.log(result);
+   i++; 
+} 
 
-first: for (let i = 0; i < 3; i++) {
-   console.log(`First level: ${i}`);
-   for (let j = 0; j < 3; j++) {
-      console.log(`Second level: ${j}`);
-      for (let k = 0; k < 3; k++) {
-         if (k === 2) {
-            continue first;
-         }
-         console.log(`Third level: ${k}`);
-      }
-   }
+// Место для пятой задачи
+const arrayOfNumbers = [];
+
+for (let i = 5; i < 11; i++) {
+   arrayOfNumbers[i - 5] = i;
 }
+
+console.log(arrayOfNumbers);
