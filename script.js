ba4a7fac92b2e19f22a8765b2976a1ddd3dfe7ae
Dmitry <d.a16@yandex.ru>
2022-04-22 12:21:53 +0300

    Loop within a loop and labels

--- script.js
+++ script.js
@@ -1,77 +1,39 @@
 "use strict"; 
 
-const hamburger = 5;
-const fries = 0;
-
-if (hamburger === 3 && fries) {
-   console.log('Я сыт!');
-} else {
-   console.log('Товар закончился');
+for (let i = 0; i < 3; i++) {
+   console.log(i);
+   for (let j = 0; j < 3; j++) {
+   console.log(j);
+   }
 }
 
-console.log((hamburger && fries));
+// *
+// **
+// ***
+// ****
+// *****
+// ******
 
-const hamburger = 3;
-const fries = 3;
-const cola = 0;
-const nuggets = 2;
+let result = '';
+const leng = 7;
 
-if (hamburger === 3 && cola === 2 || fries === 3 && nuggets) {
-   console.log('Все довольны!');
-} else {
-   console.log('Мы уходим');
+for (let i = 1; i < leng; i++) {
+   for (let j = 0; j < i; j++) {
+      result += "*";
+   }
+   result += '\n';
 }
+console.log(result);
 
-let johnReport, alexReport, samReport, mariaReport = 'done';
-
-console.log(!0);
-
-// задачи
-console.log( NaN || 2 || undefined );
- 
-console.log( NaN && 2 && undefined );
- 
-console.log( 1 && 2 && 3 );
- 
-console.log( !1 && 2 || !3 );
- 
-console.log( 25 || null && !3 );
- 
-console.log( NaN || null || !3 || undefined || 5);
- 
-console.log( NaN || null && !3 && undefined || 5);
- 
-console.log( 5 === 5 && 3 > 1 || 5);
-
-// Выполняется ли условие?
-
-const hamburger = 3;
-const fries = 3;
-const cola = 0;
-const nuggets = 2;
- 
-if (hamburger === 3 && cola || fries === 3 && nuggets) {
-   console.log('Done!')
-}
-
-// Выполняется ли условие?
-
-let hamburger;
-const fries = NaN;
-const cola = 0;
-const nuggets = 2;
- 
-if (hamburger || cola || fries === 3 || nuggets) {
-   console.log('Done!')
-}
-
-// Выполняется ли условие?
-
-let hamburger;
-const fries = NaN;
-const cola = 0;
-const nuggets = 2;
- 
-if (hamburger && cola || fries === 3 && nuggets) {
-   console.log('Done!')
+first: for (let i = 0; i < 3; i++) {
+   console.log(`First level: ${i}`);
+   for (let j = 0; j < 3; j++) {
+      console.log(`Second level: ${j}`);
+      for (let k = 0; k < 3; k++) {
+         if (k === 2) {
+            continue first;
+         }
+         console.log(`Third level: ${k}`);
+      }
+   }
 }
