355675902b4ac7fb5747c8a3dfc3b9a8706cdf29
Dmitriy <d.a16@yandex.ru>
2022-04-24 11:47:45 +0300

    The task of shaping the figure

--- script.js
+++ script.js
@@ -1,33 +1,16 @@
 "use strict"; 
 
-// Место для первой задачи
-const arr = [3, 5, 8, 16, 20, 23, 50];
-const result = [];
+const lines = 5;
+let result = '';
 
-for (let i = 0; i < arr.length; i++) {
-   result[i] = arr[i];
-}
-
-console.log(result);
-
-// Место для второй задачи
-const data = [5, 10, 'Shopping', 20, 'Homework'];
-
-for (let i = 0; i < data.length; i++) {
-    if (typeof(data[i]) === 'number') {        data[i] = data[i] * 2;
-   } else if (typeof(data[i]) === 'string') {
-      data[i] = `${data[i]} - done`;
+for (let i = 0; i <= lines; i++) {
+   for (let j = 0; j < lines - i; j++) {
+      result += " ";
    }
-}
-
-console.log(data);
-
-// Место для третьей задачи
-const data2 = [5, 10, 'Shopping', 20, 'Homework'];
-const result2 = [];
-
-for (let i = 1; i <= data2.length; i++) {
-   result2[i - 1] = data2[data2.length - i];
+    for (let j = 0; j < 2 * i + 1; j++) {
+      result += "*";
+   }
+   result += "\n";
 }
 
 console.log(result);
