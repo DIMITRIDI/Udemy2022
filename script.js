2eaf5c56e2d6c46d00a08009e3fdfbb4c3cca95d
Dmitry <d.a16@yandex.ru>
2022-04-26 12:17:47 +0300

    Code Exercise 5: Shape Problem

--- script.js
+++ script.js
@@ -1,37 +1,16 @@
 "use strict"; 
 
-// Место для первой задачи
+const lines = 5;
+let result = '';
+// Проверяется именно переменная result, формируйте строку в ней
+for (let i = 0; i <= lines; i++) {
+   for (let j = 0; j < lines - i; j++) {
+      result += " ";
+   }
+    for (let j = 0; j < 2 * i + 1; j++) {
+      result += "*";
+   }
+   result += "\n";
+}
 
-    const arr = [3, 5, 8, 16, 20, 23, 50];
-    const result = [];
-
-    for (let i = 0; i < arr.length; i++) {
-        result[i] = arr[i];
-    }
-
-    console.log(result);
-
-// Место для второй задачи
-
-    const data = [5, 10, 'Shopping', 20, 'Homework'];
-
-    for (let i = 0; i < data.length; i++) {
-        if (typeof(data[i]) === 'number') {
-            data[i] = data[i] * 2;
-        } else if (typeof(data[i]) === 'string') {
-            data[i] = `${data[i]} - done`;
-        }
-    }
-
-    console.log(data);
-
-// Место для третьей задачи
-
-    const data1 = [5, 10, 'Shopping', 20, 'Homework'];
-    const result1 = [];
-
-    for (let i = 1; i <= data1.length; i++) {
-        result1[i - 1] = data1[data1.length - i];
-    }
-
-    console.log(result1);
+console.log(result);
