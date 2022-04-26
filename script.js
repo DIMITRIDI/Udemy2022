df79f2fdd5b9f53e4ff92f708057bf42d17975f4
Dmitry <d.a16@yandex.ru>
2022-04-26 12:13:25 +0300

    Code Exercise 4: Advanced Loops and Conditions

--- script.js
+++ script.js
@@ -1,49 +1,37 @@
 "use strict"; 
 
 // Место для первой задачи
-for (let i = 5; i < 11; i++) {
-   console.log(i);
-}
+
+    const arr = [3, 5, 8, 16, 20, 23, 50];
+    const result = [];
+
+    for (let i = 0; i < arr.length; i++) {
+        result[i] = arr[i];
+    }
+
+    console.log(result);
+
 // Место для второй задачи
-for (let i = 20; i >= 10; i--) {
-   if (i === 13) {
-      break;
-   }
-   console.log(i);
-}
+
+    const data = [5, 10, 'Shopping', 20, 'Homework'];
+
+    for (let i = 0; i < data.length; i++) {
+        if (typeof(data[i]) === 'number') {
+            data[i] = data[i] * 2;
+        } else if (typeof(data[i]) === 'string') {
+            data[i] = `${data[i]} - done`;
+        }
+    }
+
+    console.log(data);
 
 // Место для третьей задачи
-for (let i = 2; i <= 10; i++) {
-   if (i % 2 === 0) {
-      console.log(i);
-   }
-}
 
-// Место для четвертой задачи
-// Цикл, который нужно переписать:
-// for (let i = 2; i <= 16; i++) {
-//     if (i % 2 === 0) {
-//         continue;
-//     } else {
-//         console.log(i);
-//     }
-// }
+    const data1 = [5, 10, 'Shopping', 20, 'Homework'];
+    const result1 = [];
 
-let i = 3; 
+    for (let i = 1; i <= data1.length; i++) {
+        result1[i - 1] = data1[data1.length - i];
+    }
 
-while (i <= 16) { 
-   if (i % 2 === 0) {
-   } else {
-      console.log(i);
-   }
-   i++; 
-} 
-
-// Место для пятой задачи
-const arrayOfNumbers = [];
-
-for (let i = 5; i < 11; i++) {
-   arrayOfNumbers[i - 5] = i;
-}
-
-console.log(arrayOfNumbers);
+    console.log(result1);
