7138512f85a081de09b37132e7e04e8d3e52114e
Dmitry <d.a16@yandex.ru>
2022-04-28 16:00:10 +0300

    Code Exercise 8: Advanced Function Tasks

--- script.js
+++ script.js
@@ -1,40 +1,50 @@
 'use strict';
 
-/* 1. Создайте функцию, которая будет вычислять объем и площадь полной поверхности куба. Эта функция принимает в себя целое число со значением длины ребра куба. Ответ выведите в формате строки, который изображен в примерах.
-Если в функцию попал неправильный аргумент или вычислить значение невозможно - вернуть строку "При вычислении произошла ошибка". */
-function calculateVolumeAndArea(length) {
-   if (typeof(length) !== 'number' || length < 0 || !Number.isInteger(length)) {
-      return "При вычислении произошла ошибка";
+// 1. Создайте функцию, которая принимает в себя целое число минут и возвращает время в нужном формате строки. Обратите внимание на окончание слова "час" - оно меняется в зависимости от цифры. Если вместо аргумента приходит не число, дробное или отрицательное число - функция возвращает строку "Ошибка, проверьте данные".
+function getTimeFromMinutes(minutesTotal) {
+   if (typeof(minutesTotal) !== 'number' || minutesTotal < 0 || !Number.isInteger(minutesTotal)) {
+      return "Ошибка, проверьте данные";
    }
 
-   let volume = 0,
-      area = 0;
-   
-    volume = length * length * length;
-    /* length ** 3 - это тоже самое, что и выше или варианты через цикл.
-    ** - это оператор степени, напоминаю. Но онлайн редактор его не принимает */
-    area = 6 * (length * length);
+   const hours = Math.floor(minutesTotal / 60);
+   const minutes = minutesTotal % 60;
 
-   return `Объем куба: ${volume}, площадь всей поверхности: ${area}`;
+   let hoursStr = '';
+
+   switch (hours) {
+      case 0: 
+         hoursStr = 'часов';
+         break;
+      case 1:
+         hoursStr = 'час';
+         break;
+      case 2:
+      case 3:
+      case 4:
+         hoursStr = 'часа';
+         break;
+      default:
+         hoursStr = 'часов';
+   }
+
+   return `Это ${hours} ${hoursStr} и ${minutes} минут`;
 }
 
-calculateVolumeAndArea(5);
+getTimeFromMinutes(180);
 
-// 2. Напишите функцию, которая будет определять номер купе по номеру переданного ей места
-function getCoupeNumber(seatNumber) {
-   if (typeof(seatNumber) !== 'number' || seatNumber < 0 || !Number.isInteger(seatNumber)) {
-      return "Ошибка. Проверьте правильность введенного номера места";
-   }
-
-   if (seatNumber === 0 || seatNumber > 36) {
-      return "Таких мест в вагоне не существует";
-   }
-
-   for (let i = 4; i <= 36; i = i + 4) {
-      if (seatNumber <= i) {
-            return Math.ceil(i / 4);
-      }
+// 2. Напишите функцию, которая принимает в себя 4 числа и возвращает самое большее из них. Если один из аргументов не является числом - возвращается - 0. Дробные числа разрешены.
+function findMaxNumber(a, b ,c, d) {
+    // Самое простое - это использовать Math.max :)
+    // А оптимизировать такую проверку мы научимся совсем скоро
+   if (typeof(a) !== 'number' ||
+      typeof(b) !== 'number' ||
+      typeof(c) !== 'number' ||
+      typeof(d) !== 'number') {
+      return 0;
+   } else {
+      return Math.max(a, b ,c, d);
    }
 }
 
-getCoupeNumber(33);
+findMaxNumber(1, 5, 6.6, 10.5);
+findMaxNumber(1, 5, '6', '10');
