22e30a8e08d037f753dba8fdaacec30585824ab1
Dmitry <d.a16@yandex.ru>
2022-04-28 15:36:07 +0300

    Code Exercise 7: Advanced Function Tasks

--- script.js
+++ script.js
@@ -1,78 +1,40 @@
-/* Задание на урок:
-1) Первую часть задания повторить по уроку
-2) Создать функцию showMyDB, которая будет проверять свойство privat. Если стоит в позиции
-false - выводит в консоль главный объект программы
-3) Создать функцию writeYourGenres в которой пользователь будет 3 раза отвечать на вопрос 
-"Ваш любимый жанр под номером ${номер по порядку}". Каждый ответ записывается в массив данных
-genres
-P.S. Функции вызывать не обязательно */
-
 'use strict';
 
-let numberOfFilms;
-
-function start() {
-   numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
-
-   while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
-      numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+/* 1. Создайте функцию, которая будет вычислять объем и площадь полной поверхности куба. Эта функция принимает в себя целое число со значением длины ребра куба. Ответ выведите в формате строки, который изображен в примерах.
+Если в функцию попал неправильный аргумент или вычислить значение невозможно - вернуть строку "При вычислении произошла ошибка". */
+function calculateVolumeAndArea(length) {
+   if (typeof(length) !== 'number' || length < 0 || !Number.isInteger(length)) {
+      return "При вычислении произошла ошибка";
    }
+
+   let volume = 0,
+      area = 0;
+   
+    volume = length * length * length;
+    /* length ** 3 - это тоже самое, что и выше или варианты через цикл.
+    ** - это оператор степени, напоминаю. Но онлайн редактор его не принимает */
+    area = 6 * (length * length);
+
+   return `Объем куба: ${volume}, площадь всей поверхности: ${area}`;
 }
 
-start();
+calculateVolumeAndArea(5);
 
-const personalMovieDB = {
-   count: numberOfFilms,
-   movies: {},
-   actors: {},
-   genres: [],
-   privat: true
-};
+// 2. Напишите функцию, которая будет определять номер купе по номеру переданного ей места
+function getCoupeNumber(seatNumber) {
+   if (typeof(seatNumber) !== 'number' || seatNumber < 0 || !Number.isInteger(seatNumber)) {
+      return "Ошибка. Проверьте правильность введенного номера места";
+   }
 
+   if (seatNumber === 0 || seatNumber > 36) {
+      return "Таких мест в вагоне не существует";
+   }
 
-function rememberMyFilms() {
-   for (let i = 0; i < 2; i++) {
-      const a = prompt('Один из последних просмотренных фильмов?', ''),
-            b = prompt('На сколько оцените его?', '');
-
-      if (a != null && b != null && a != '' && b != '' && a.length < 50) {
-         personalMovieDB.movies[a] = b;
-         console.log('done');
-      } else {
-         console.log('error');
-         i--;
+   for (let i = 4; i <= 36; i = i + 4) {
+      if (seatNumber <= i) {
+            return Math.ceil(i / 4);
       }
    }
 }
 
-rememberMyFilms();
-
-function detectPersonalLevel() {
-   if (personalMovieDB.count < 10) {
-      console.log("Просмотрено довольно мало фильмов");
-   } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
-      console.log("Вы классический зритель");
-   } else if (personalMovieDB.count >= 30) {
-      console.log("Вы киноман");
-   } else {
-      console.log("Произошла ошибка");
-   }
-}
-
-detectPersonalLevel();
-
-function showMyDB (hidden) {
-   if (!hidden) {
-      console.log(personalMovieDB);
-   }
-}
-
-showMyDB(personalMovieDB.privat);
-
-function writeYourGenres() {
-   for (let i = 1; i <= 3; i++) {
-      personalMovieDB.genres[i - 1] = prompt(`Ваш любимый жанр под номером ${i}`);
-   }
-}
-
-writeYourGenres();
+getCoupeNumber(33);
