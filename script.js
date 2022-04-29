ceb8fd05cc8f54d75fc9fb6d5bbe0eb22d7bcabc
Dmitry <d.a16@yandex.ru>
2022-04-29 12:31:02 +0300

    Callback functions

--- script.js
+++ script.js
@@ -1,28 +1,26 @@
 'use strict';
 
-function fib(num) {
-   if (typeof(num) !== 'number' || num <= 0 || !Number.isInteger(num)) {
-      return "";
-   }
-
-   let result = '';
-   let first = 0;
-   let second = 1;
-
-   for (let i = 0; i < num; i++) {
-      if (i + 1 === num) {
-            result += `${first}`;
-            // Без пробела в конце
-      } else {
-            result += `${first} `;
-      }
-
-      let third = first + second;
-      first = second;
-      second = third;
-   }
-
-   return result;
+function first() {
+   // Do something
+   setTimeout(function() {
+      console.log(1);
+   }, 500);
 }
 
-fib(5)
+function  second() {
+   console.log(2);
+}
+
+first();
+second();
+
+function learnJS(lang, callback) {
+   console.log(`Я учу: ${lang}`);
+   callback();
+}
+
+function done() {
+   console.log('Я прошел этот урок!');
+}
+
+learnJS('JavaScript', done);
