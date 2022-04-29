2d83cb7577afb63d3d16186ad66c44eb9de0205c
Dmitry <d.a16@yandex.ru>
2022-04-29 11:14:34 +0300

    Code Exercise 9: Fibonacci Interview Problem

--- script.js
+++ script.js
@@ -1,50 +1,28 @@
 'use strict';
 
-// 1. Создайте функцию, которая принимает в себя целое число минут и возвращает время в нужном формате строки. Обратите внимание на окончание слова "час" - оно меняется в зависимости от цифры. Если вместо аргумента приходит не число, дробное или отрицательное число - функция возвращает строку "Ошибка, проверьте данные".
-function getTimeFromMinutes(minutesTotal) {
-   if (typeof(minutesTotal) !== 'number' || minutesTotal < 0 || !Number.isInteger(minutesTotal)) {
-      return "Ошибка, проверьте данные";
+function fib(num) {
+   if (typeof(num) !== 'number' || num <= 0 || !Number.isInteger(num)) {
+      return "";
    }
 
-   const hours = Math.floor(minutesTotal / 60);
-   const minutes = minutesTotal % 60;
+   let result = '';
+   let first = 0;
+   let second = 1;
 
-   let hoursStr = '';
+   for (let i = 0; i < num; i++) {
+      if (i + 1 === num) {
+            result += `${first}`;
+            // Без пробела в конце
+      } else {
+            result += `${first} `;
+      }
 
-   switch (hours) {
-      case 0: 
-         hoursStr = 'часов';
-         break;
-      case 1:
-         hoursStr = 'час';
-         break;
-      case 2:
-      case 3:
-      case 4:
-         hoursStr = 'часа';
-         break;
-      default:
-         hoursStr = 'часов';
+      let third = first + second;
+      first = second;
+      second = third;
    }
 
-   return `Это ${hours} ${hoursStr} и ${minutes} минут`;
+   return result;
 }
 
-getTimeFromMinutes(180);
-
-// 2. Напишите функцию, которая принимает в себя 4 числа и возвращает самое большее из них. Если один из аргументов не является числом - возвращается - 0. Дробные числа разрешены.
-function findMaxNumber(a, b ,c, d) {
-    // Самое простое - это использовать Math.max :)
-    // А оптимизировать такую проверку мы научимся совсем скоро
-   if (typeof(a) !== 'number' ||
-      typeof(b) !== 'number' ||
-      typeof(c) !== 'number' ||
-      typeof(d) !== 'number') {
-      return 0;
-   } else {
-      return Math.max(a, b ,c, d);
-   }
-}
-
-findMaxNumber(1, 5, 6.6, 10.5);
-findMaxNumber(1, 5, '6', '10');
+fib(5)
