f132b2075f30b994a11952827a95c80b64172410
Dmitriy <d.a16@yandex.ru>
2022-05-01 19:23:17 +0300

    Code Exercise 10: Object Tasks

--- script.js
+++ script.js
@@ -1,115 +1,46 @@
 "use strict";
 
-const obj = {
-   a: 5,
-   b: 1
+const personalPlanPeter = {
+   name: "Peter",
+   age: "29",
+   skills: {
+      languages: ['ru', 'eng'],
+      programmingLangs: {
+         js: '20%',
+         php: '10%'
+      },
+      exp: '1 month'
+   },
+   showAgeAndLangs: function(plan) {
+      const {age} = plan;
+      const {languages} = plan.skills;
+      let str = `Мне ${age} и я владею языками: `;
+
+      languages.forEach(function(lang) {
+         str += `${lang.toUpperCase()} `;
+      });
+
+      return str;
+   }
 };
 
-const copy = obj; //Ссылка на уже существующий объект
+personalPlanPeter.showAgeAndLangs(personalPlanPeter);
 
-copy.a = 10;
-
-console.log(copy);
-console.log(obj);
-
-function copy1(mainObj) {
-   let objCopy = {};
-
-   let key;
-   for (key in mainObj) {
-      objCopy[key] = mainObj[key];
-   }
-
-   return objCopy;
+function showExperience(plan) {
+   const {exp} = plan.skills;
+   return exp;
 }
 
-const numbers = {
-   a: 2,
-   b: 5,
-   c: {
-      x: 7,
-      y: 4
-   }
-};
+showExperience(personalPlanPeter);
 
-const newNumbers = copy1(numbers); // клонирование объекта, поверхностная копия
-
-newNumbers.a = 10;
-newNumbers.c.x = 10;
-
-console.log(newNumbers);
-console.log(numbers);
-
-function copy2(mainObj) {
-   let objCopy2 = {};
-
-   let key;
-   for (key in mainObj) {
-      objCopy2[key] = mainObj[key];
+function showProgrammingLangs(plan) {
+   let str = '';
+   const {programmingLangs} = plan.skills;
+   for (let key in programmingLangs) {
+      str += `Язык ${key} изучен на ${programmingLangs[key]}\n`;
    }
 
-   return objCopy2;
+   return str;
 }
 
-const numbers2 = {
-   a: 2,
-   b: 5,
-   c: {
-      x: 7,
-      y: 4
-   }
-};
-
-const newNumbers2 = copy2(numbers2);
-
-newNumbers2.a = 10;
-newNumbers2.c.x = 10;
-
-console.log(newNumbers2);
-console.log(numbers2);
-
-const add = {
-   d: 17,
-   e: 20
-};
-
-console.log(Object.assign(numbers2, add));
-
-// Создадим копию массива методом slice()
-const oldArray = ['a', 'b', 'c'];
-const newArray = oldArray.slice();
-
-newArray[1] = 'adgsghfk';
-
-console.log(newArray);
-console.log(oldArray);
-
-const video = ['youtube', 'vimeo', 'rutube'],
-      blogs = ['wordpress', 'livejournal', 'blogger'],
-      internet = [...video, ...blogs, 'vk', 'facebook'];
-
-console.log(internet);
-// оператор разворота развернул массивы на отдельные элементы
-
-function log(a, b, c) {
-   console.log(a);
-   console.log(b);
-   console.log(c);
-}
-
-const num = [2, 5, 7];
-
-log(...num); // Три аргумента передаем в функцию log
-
-// Создаем копию массива с помощью спред-оператора
-const array = ["a", "b"];
-
-const newAarray = [...array];
-
-// Создаем копию объекта с помощью спред-оператора
-const q = {
-   one: 1,
-   two: 2
-};
-
-const newObj = {...q};
+showProgrammingLangs(personalPlanPeter);
