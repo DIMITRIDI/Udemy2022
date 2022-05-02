55631b437a647e96fdd70af03db2d8e3d1db6e93
Dmitriy <d.a16@yandex.ru>
2022-05-02 20:57:07 +0300

    OOP basics, prototypal inheritance

--- script.js
+++ script.js
@@ -1,47 +1,13 @@
 "use strict";
 
-const someString = 'This is some strange string';
-
-function reverse(str) {
-   if (typeof(str) !== 'string') {
-      return "Ошибка!";
+const solder = {
+   health: 400,
+   armor: 100,
+   sayHello: function() {
+      console.log("Hello");
    }
-    // Самый оптимальный вариант решения
-   return str.split('').reverse().join('');
+};
 
-    // Решение при помощи цикла
-    // let newStr = '';
-    // for (let i = str.length - 1; i >= 0; i--) {
-    //     newStr += str[i];
-    // }
-    // return newStr
-}
+const jonh = Object.create(solder);
 
-reverse(someString);
-
-const baseCurrencies = ['USD', 'EUR'];
-const additionalCurrencies = ['UAH', 'RUB', 'CNY'];
-
-function availableCurr(arr, missingCurr) {
-   let str = '';
-   arr.length === 0 ? str = 'Нет доступных валют' : str = 'Доступные валюты:\n';
-
-   arr.forEach(function(curr, i) {
-      if (curr !== missingCurr) {
-            str += `${curr}\n`;
-      }
-   });
-
-    // Или
-    // for (let i = 0; i < arr.length; i++) {
-    //     if (arr[i] === missingCurr) {
-    //         continue;
-    //     }
-    //     str += `${arr[i]}\n`;
-    // }
-
-   return str;
-}
-
-availableCurr([...baseCurrencies, ...additionalCurrencies], 'CNY');
-console.log(availableCurr([...baseCurrencies, ...additionalCurrencies], 'CNY'));
+jonh.sayHello();
