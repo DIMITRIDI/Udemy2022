4bdf186c66f7957b1f322e4762907ec4516c6d20
Dmitry <d.a16@yandex.ru>
2022-05-04 11:35:56 +0300

    Closure and Lexical Environment

--- script.js
+++ script.js
@@ -1,42 +1,30 @@
 'use strict';
 
-// To string
-// 1)
-console.log(typeof(String(null)));
-console.log(typeof(String(4)));
+let number = 5;
 
-// 2)
-const num = 5;
-
-console.log("https://vk.com/catalog/" + num);
-
-const fontSize = 26 + 'px';
-
-// To Number
-// 1)
-console.log(typeof(Number(null)));
-
-// 2) unary plus
-console.log(typeof(+'5'));
-
-// 3) parseInt()
-console.log(typeof(parseInt("15px", 10)));
-
-let answ = +prompt("Hello", "");
-
-// To boolean
-// always false: 0, '', null, undefined, NaN
-// always true: (пустые переменные, пустые массивы и т.п.)
-
-// 1)
-let switcher = null;
-
-if (switcher) {
-   console.log('Working...');
+function logNumber() {
+   let number = 4;
+   console.log(number);
 }
 
-// 2)
-console.log(typeof(Boolean('4')));
+number = 6;
 
-// 3)
-console.log(typeof(!!('4')));
+logNumber();
+
+function createCounter() {
+   let counter = 0;
+
+   const myFunction = function() { debugger
+      counter = counter + 1; debugger
+      return counter; debugger
+   };
+
+   return myFunction;
+}
+debugger
+const increment = createCounter(); debugger
+const c1 = increment(); debugger
+const c2 = increment(); debugger
+const c3 = increment(); debugger
+
+console.log(c1, c2, c3);
