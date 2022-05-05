bf5d3b20e530d39a123c2c94a63c3d0bb6e5dd7d
Dmitry <d.a16@yandex.ru>
2022-05-05 14:59:57 +0300

    Actions with elements on the page

--- js/script.js
+++ js/script.js
@@ -1,23 +1,54 @@
 'use strict';
 
-const box = document.getElementById('box');
+const box = document.getElementById('box'),
+      btns = document.getElementsByTagName('button'),
+      circles = document.getElementsByClassName('circle'),
+      hearts = document.querySelectorAll('.heart'),
+      oneHeart = document.querySelector('.heart'),
+      wrapper = document.querySelector('.wrapper');
 
-console.log(box);
+box.style.backgroundColor = 'blue';
+box.style.width = '500px';
 
-const btns = document.getElementsByTagName('button');
+btns[1].style.borderRadius = '100%';
 
-console.log(btns[1]);
+box.style.cssText = 'background-color: blue; width: 500px';
 
-const circles = document.getElementsByClassName('circle');
-
-console.log(circles);
-
-const hearts = document.querySelectorAll('.heart');
+for (let i = 0; i < hearts.length; i++) {
+   hearts[i].style.backgroundColor = 'blue';
+}
 
 hearts.forEach(item => {
-   console.log(item);
+   item.style.backgroundColor = 'blue';
 });
 
-const oneHeart = document.querySelector('.heart');
+const div = document.createElement('div');
+const text = document.createTextNode('Тут был я');
 
-console.log(oneHeart);
+div.classList.add('black');
+
+document.body.append(div);
+document.body.append(text);
+
+wrapper. prepend(div);
+
+hearts[0].after(div);
+
+circles[0].remove();
+
+hearts[0].replaceWith(circles[0]);
+
+// Команды, которые устарели
+wrapper.appendChild(div);
+
+wrapper.insertBefore(div, hearts[0]);
+
+wrapper.removeChild(hearts[1]);
+
+wrapper.replaceChild(circles[0], hearts[0]);
+
+div.innerHTML = "<h1>Hello World</h1>";
+
+div.textContent = "Hello";
+
+div.insertAdjacentHTML('afterbegin', '<h2>Hello</h2>');
