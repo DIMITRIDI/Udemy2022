a7db8ed653d6fd4a6860e7b15b64ecf27eb9af93
Dmitriy <d.a16@yandex.ru>
2022-05-08 12:05:04 +0300

    Null Merge Operator (Nullish, ??) ES11

--- script.js
+++ script.js
@@ -1,37 +1,19 @@
-// touchstart - при начальном касании элемента
-// touchmove - при движении пальца по элементу
-// touchend - при отрыве пальца от элемента
-// touchenter - когда мы ведем по экрану и попадаем на элемент, на который навешено это событие
-// touchleave - когда палец продолжил движение по экрану и ушел за пределы элемента с событием
-// touchcancel - когда точка сопрекосновения больше не регистрируется на поверхности экрана
+'use strict';
 
-window.addEventListener('DOMContentLoaded', () => {
-   const box = document.querySelector('.box');
+const box = document.querySelector('.box');
 
-   box.addEventListener('touchstart', (e) => {
-      e.preventDefault();
+const newHeight = 100;
+const newWidth = 400;
 
-      console.log('Start');
+function changeParams(elem, h, w) {
+   elem.style.height = `${h ?? 200}px`;
+   elem.style.width = `${w ?? 200}px`;
+   elem.innerHTML = (h ?? 200) * (w ?? 200);
+}
 
-      // touches - список всех пальцев, которые взаимодействуют с экраном
-      console.log(e.touches);
+changeParams(box, newHeight, newWidth);
 
-      // targetTouches - количество пальцев взаимодействующих с конкретным элементом
-      console.log(e.targetTouches[0].pageX); // отслеживание координат при движении пальца
+let userName;
+let userKey;
 
-      // changedTouches - список пальцев, участвующих в текущем событии
-
-   });
-
-   box.addEventListener('touchmove', (e) => {
-      e.preventDefault();
-
-      console.log('Move');
-   });
-
-   box.addEventListener('touchend', (e) => {
-      e.preventDefault();
-
-      console.log('End');
-   });
-});
+console.log(userName ?? userKey ?? 'User');
