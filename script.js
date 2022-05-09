fd9291f764350d076fc3ba54cb31372df8b12c38
Dmitriy <d.a16@yandex.ru>
2022-05-09 12:23:07 +0300

    Iterable constructs

--- script.js
+++ script.js
@@ -3,40 +3,65 @@
 const user = {
    name: 'Alex',
    surname: 'Smith',
-   [Symbol('birthday')]: '20/04/2021',
    birthday: '20/04/1993',
    showMyPublicData: function () {
       console.log(`${this.name} ${this.surname}`);
    }
 };
 
-// writable - если true - свойство можно изменить, если false - то оно только для чтения
-// enumerable - если true - свойство будет перечисляться в цикл, если false - то циклы его будут игнорировать
-// configurable - если true - свойство можно удалить, а его атрибуты изменить, если false - делать это нельзя
-// В консоли все эти флаги стоят по умолчанию в позиции true
-// в консоль выведем эти флаги, для этого нужно воспользоваться командой getOwnPropertyDescriptor
-console.log(Object.getOwnPropertyDescriptor(user, 'name')); 
-
-Object.defineProperty(user, 'name', {writable: false});
-// user.name = 'dfkgkj'; // в консоли получаем ошибку, свойство можно читать, но нельзя изменять
-
-Object.defineProperty(user, 'gender', {value: 'male'}); // создадим новое свойство gender
-console.log(Object.getOwnPropertyDescriptor(user, 'gender')); // убедимся что все флаги false
-
-// Задача: когда пользователь заполняет свои данные, то дату рождения он может указать только 1 раз, изменить ее нельзя
-Object.defineProperty(user, 'birthday', {writable: false});
-console.log(Object.getOwnPropertyDescriptor(user, 'birthday')); 
-
-// Задача: необходимо куда-либо выводить данные из объекта
-Object.defineProperty(user, 'showMyPublicData', {enumerable: false}); // данный метод не будет перечисляться в цикле
 for (let key in user) {
-   console.log(Object.getOwnPropertyDescriptor(user, "[Symbol('birthday')]"));
-   console.log(key); // в консоли получим функцию showMyPublicData из изначального объекта
+   console.log(user[key]);
 }
 
-console.log(Object.getOwnPropertyDescriptor(Math, 'PI')); // получим объект с неизменяемыми свойствами
+const arr = ['b', 'a', 'c'];
+Array.prototype.someMethod = function () {};
 
-Object.defineProperties(user, { // способ позволяющий изменять все необходимые свойства
-   name: {writable: false},
-   surname: {writable: false}
-});
+console.dir(arr);
+
+for (let key in arr) { // получает ключ
+   console.log(key);
+}
+
+for (let key of arr) { // сразу получает значение
+   console.log(key);
+}
+
+const str = 'string';
+
+for (let key in str) {
+   console.log(str[key]);
+}
+
+const salaries = {
+   john: 500,
+   ivan: 1000,
+   ann: 5000,
+   sayHello: function() {
+      console.log('Hello');
+   }
+};
+
+salaries[Symbol.iterator] = function () {
+   return {
+      current: this.john,
+      last: this.ann,
+
+      next() {
+         if (this.current < this.last) {
+            this.current = this.current + 500;
+            return {done: false, value: this.current}; // цикл еще работает
+         } else {
+            return {done: true}; // цикл завершен
+         }
+
+         // {done: true, value: 123}
+      }
+   };
+};
+
+const iterator = salaries[Symbol.iterator](); // так как это метод, мы его сразу вызываем
+console.log(iterator.next());
+
+// for (let res of salaries) {
+   // console.log(res);
+// }
