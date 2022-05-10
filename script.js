4e782bdde4cdc56c96067ff401eed5ac048ea455
Dmitriy <d.a16@yandex.ru>
2022-05-10 11:33:38 +0300

    Code Exercise 18: Hard String Problem

--- script.js
+++ script.js
@@ -1,16 +1,46 @@
 'use strict';
 
-function amountOfPages(summary){
-   let result = '';
-   let n = 0;
+// Вариант, когда строка переводится в нижний регистр до всех операций только 1 раз
+// Это должно экономить ресурсы компьютера
+function isPangram(string){
+   string = string.toLowerCase();
+   return "abcdefghijklmnopqrstuvwxyz".split("").every(function(x){
+      return string.indexOf(x) !== -1;
+   });
+}
 
-   for (let i = 1; i <= summary; i++) {
-      result += i;
-      if (result.length === summary) {
-         n = i;
-         break;
+// С другим методом и строка каждый раз преобразовывается в коллбэке
+function isPangram(string){
+   return 'abcdefghijklmnopqrstuvwxyz'
+      .split('')
+      .every((x) => string.toLowerCase().includes(x));
+}
+
+// При помощи цикла
+function isPangram(str) {
+   letters: for (var c = 0; c < 26; c++) {
+      for (let i = 0; i < str.length; i++) {
+         let s = str.charCodeAt(i);
+         if (s < 65 || s > 90 && s < 97 || s > 122) {
+            continue;
+         }
+         if (s === 65 + c || s === 97 + c) {
+            continue letters;
+         }
       }
+      
+      return false;
    }
+   
+   return true;
+}
 
-   return n;
+// При помощи Set
+function isPangram(string) {
+   return new Set(string.toLocaleLowerCase().replace(/[^a-z]/gi, '').split('')).size === 26;
+}
+
+// С использованием регулярных выражений
+function isPangram(string){
+   return (string.match(/([a-z])(?!.*\1)/ig) || []).length === 26;
 }
