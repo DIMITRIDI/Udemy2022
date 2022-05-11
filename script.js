d8526bf86de328c80d15147aa1897fdd2c7efddb
Dmitry <d.a16@yandex.ru>
2022-05-11 11:56:37 +0300

    Garbage collector and memory leaks

--- script.js
+++ script.js
@@ -1,51 +1,36 @@
-const btn1 = document.querySelector('.btn');
-
-btn1.addEventListener('click', () => {
-   const timerId = setTimeout(logger, 2000);
-});
-
-// const timerId = setTimeout(logger, 2000); 
-// асинхронные функции задаються в переменной, чтоб иметь возможность их останавливать
-
-// function logger () {
-//    console.log('text');
-// }
-
-const btn = document.querySelector('.btn');
-let timerId,
-   i = 0;
-
-btn.addEventListener('click', () => {
-   timerId = setInterval(logger, 2000);
-});
-
-function logger () {
-   if (i === 3) {
-      clearInterval(timerId);
-   }
-   console.log('text');
-   i++;
+function func() {
+   window.smth = 'string'; // создание лишней глобальной переменной
 }
 
-let id = setTimeout(function log() {
-   console.log('Hello');
-   id = setTimeout(log, 500);
-}, 500); // setTimeout  выжидает 500мс и только после этого запускает внутреннюю функцию
+const someRes = getData();
+const node = document.querySelector('.class');
 
-function myAnimation() {
-   const elem = document.querySelector('.box');
-   let pos = 0;
-
-   const id = setInterval(frame, 10);
-   function frame() {
-      if (pos == 300) {
-         clearInterval(id);
-      } else {
-         pos++;
-         elem.style.top = pos + "px";
-         elem.style.left = pos + "px";
-      }
+setInterval(function() {
+   if (node) {
+      node.innerHTML = someRes;
    }
+}, 1000); // не забывайте останавливать ненужные таймеры, происходит утечка памяти
+
+function outer() {
+   const potentiallyHugeArray = [];
+   return function inner() {
+      potentiallyHugeArray.push('Hello');
+      console.log('Hello!!');
+   };
 }
 
-btn.addEventListener('click', myAnimation);
+const sayHello = outer(); // в переменной sayHello содержится ссылка на огромный массив potentiallyHugeArray
+
+function createElement() {
+   const div = document.createElement('div');
+   div.id = 'test';
+   document.body.append(testDiv);
+}
+
+createElement(); // в переменной testDiv лежит блок с установлнным id
+
+function deleteElement() {
+   document.body.removeChild(document.getElementById('test'));
+}
+
+deleteElement();
