db146fd06d19bf62fb66a038f45b0f91b37eca6e
Dmitry <d.a16@yandex.ru>
2022-05-12 12:01:31 +0300

    Create a countdown timer on the website

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 window.addEventListener('DOMContentLoaded', () => {
-
+// tabs
    const tabs = document.querySelectorAll('.tabheader__item'),
          tabsContent = document.querySelectorAll('.tabcontent'),
          tabsParent = document.querySelector('.tabheader__items');
@@ -36,5 +36,60 @@ window.addEventListener('DOMContentLoaded', () => {
          });
       }
    });
+
+// timer
+   const deadLine = '2022-12-31';
+
+   function getTimeRemaining(endtime) { // получаем разницу между дедлайн и текущей датой в милисекундах
+      const t = Date.parse(endtime) - Date.parse(new Date()),
+            days = Math.floor(t / (1000 * 60 * 60 * 24)), // полученную разницу в миллисекундах преобразовываем в дни
+            hours = Math.floor((t / (1000 * 60 * 60) % 24)), // полученную разницу в миллисекундах преобразов в часы
+            minutes = Math.floor((t / 1000 / 60) % 60), // полученную разницу в миллисекундах преобразовываем в минуты
+            seconds = Math.floor((t / 1000) % 60); // полученную разницу в миллисекундах преобразовываем в секунды
+      return { // возвращаем объект
+         'total': t,
+         'days': days,
+         'hours': hours,
+         'minutes': minutes,
+         'seconds': seconds
+      };
+   }
+
+   function getZero(num) { // Подставим ноль в показания дней и часов если там одинарное число
+      if (num >= 0 && num < 10) {
+         return `0${num}`;
+      } else {
+         return num;
+      }
+   }
+
+
+      function setClock(selector, endtime) { // установим часы на странице
+      const timer = document.querySelector(selector),
+            days = timer.querySelector('#days'),
+            hours = timer.querySelector('#hours'),
+            minutes = timer.querySelector('#minutes'),
+            seconds = timer.querySelector('#seconds'),
+            timeInterval = setInterval(updateClock, 1000); // функция запускается каждую секунду
+
+      updateClock(); // в ручную запускаем updateClock чтоб убрать мигание верстки при запуске, 
+      // а далее она будет сама срабатывать через 1000 мс
+      
+      function updateClock() { // функция обновления таймера каждую секунду
+         const t = getTimeRemaining(endtime); // расчет оставшегося времени на текущую секунду
+
+            days.innerHTML = getZero(t.days); // выводим на страницу дни, часы, минуты и секунды
+            hours.innerHTML = getZero(t.hours);
+            minutes.innerHTML = getZero(t.minutes);
+            seconds.innerHTML = getZero(t.seconds);
+
+            if (t.total <= 0) { // если время вышло <= 0, то мы больше таймер не обновляем
+               clearInterval(timeInterval);
+            }
+
+      }
+   }
+
+   setClock('.timer', deadLine);
 
 });
