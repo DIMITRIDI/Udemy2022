2dfdeb010f64940c1994f202805ea792c8fe4a85
Dmitry <d.a16@yandex.ru>
2022-05-13 15:05:01 +0300

    Constructor functions

--- script.js
+++ script.js
@@ -1,13 +1,39 @@
 'use strict';
 
-const box = document.querySelector('.box');
+function User(name, id) { // функция-конструктор, с ее помощью можно создавать новых пользователей
+   this.name = name;
+   this.id = id;
+   this.human = true;
+   this.hello = function() { // кроме свойств также можно записать и методы
+      console.log(`Hello ${this.name}`);
+   };
+}
 
-let observer = new MutationObserver(mutationRecords => { // следим за observer
-   console.log(mutationRecords); // какие действия будем предпринимать
-});
+class User1 {
+   constructor(name, id) {
+      this.name = name;
+      this.id = id;
+      this.human = true;
+   }
+   hello() {
+      console.log(`Hello! ${this.name}`);
+   }
+   exit() {
+      console.log(`Пщльзователь ${this.name} ушел`);
+   }
+}
 
-observer.observe(box, { // выбираем объект за каким нужно следить
-   childList: true // выбираем за какими изменениями нужно следить
-});
+User.prototype.exit = function() { // данный метод будет прототипно наследоваться и он появится у всех потомков
+   console.log(`Пользователь ${this.name} ушел`);
+};
 
-observer.disconnect(); // завершение слежения за объектом
+const ivan = new User('Ivan', 20); // внутри переменной ivan находится объект
+const alex = new User('Alex', 28);
+
+ivan.exit();
+
+ivan.hello();
+alex.hello();
+
+console.log(ivan);
+console.log(alex);
