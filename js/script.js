d96c6e5a2b52f51d0d69dabc297b2bb86fcb2aa6
Dmitry <d.a16@yandex.ru>
2022-05-14 20:19:30 +0300

    Putting the Rest operator into practice

--- js/script.js
+++ js/script.js
@@ -155,12 +155,13 @@ window.addEventListener('DOMContentLoaded', () => {
 
    // используем классы для карточек
    class MenuCard {
-      constructor(src, alt, title, descr, price, parentSelector) {
+      constructor(src, alt, title, descr, price, parentSelector, ...classes) { // добавим Rest-оператор classes
          this.src = src;
          this.alt = alt;
          this.title = title;
          this.descr = descr;
          this.price = price;
+         this.classes = classes; // запишем Rest-оператор в свойства чтоб его можно было использовать
          this.parent = document.querySelector(parentSelector); // родитель, куда передаем верстку
          this.transfer = 27; // пока статический курс валют
          this.changeToUAH(); // метод вызываем прямо в конструкторе
@@ -172,16 +173,22 @@ window.addEventListener('DOMContentLoaded', () => {
 
       render() { // метод для формирования верстки
          const element = document.createElement('div'); // создаем элемент
+         
+         if (this.classes.length === 0) { // условие ксли пользователь забудет прописать класс
+            this.element = 'menu__item'; // если ни один элемент в классы не передан, 
+            element.classList.add(this.element); // то установим дефолтный класс
+         } else { // если же есть хотя бы 1 класс, то запускаем forEach, добавляющий этот класс во все карточки
+            this.classes.forEach(className => element.classList.add(className));
+         }
+
          element.innerHTML = `
-            <div class="menu__item">
-               <img src=${this.src} alt=${this.alt}>
-               <h3 class="menu__item-subtitle">${this.title}</h3>
-               <div class="menu__item-descr">${this.descr}</div>
-               <div class="menu__item-divider"></div>
-               <div class="menu__item-price">
-                  <div class="menu__item-cost">Цена:</div>
-                  <div class="menu__item-total"><span>${this.price}</span> грн/день</div>
-               </div>
+            <img src=${this.src} alt=${this.alt}>
+            <h3 class="menu__item-subtitle">${this.title}</h3>
+            <div class="menu__item-descr">${this.descr}</div>
+            <div class="menu__item-divider"></div>
+            <div class="menu__item-price">
+               <div class="menu__item-cost">Цена:</div>
+               <div class="menu__item-total"><span>${this.price}</span> грн/день</div>
             </div>
          `;
          this.parent.append(element);
@@ -194,7 +201,8 @@ window.addEventListener('DOMContentLoaded', () => {
       "Меню 'Фитнес'",
       "Меню 'Фитнес' - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством массового потребления!",
       9,
-      ".menu .container"
+      ".menu .container",
+      "menu__item"
    ).render(); // вызываем класс MenuCard и передаем в него аргументы
 
    new MenuCard(
@@ -203,7 +211,8 @@ window.addEventListener('DOMContentLoaded', () => {
       "Меню 'Премиум'",
       "В меню 'Премиум' мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд лучшими нашими поварами. Красная рыба, морепродукты, овощи, грибы, фрукты - ресторанное меню без похода в ресторан!",
       14,
-      ".menu .container"
+      ".menu .container",
+      "menu__item"
    ).render(); // вызываем класс MenuCard и передаем в него аргументы
 
    new MenuCard(
@@ -212,6 +221,7 @@ window.addEventListener('DOMContentLoaded', () => {
       "Меню 'Постное'",
       "Меню 'Постное' - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное количество белков за счет тофу и импортных вегетарианских стейков.",
       21,
-      ".menu .container"
+      ".menu .container",
+      "menu__item"
    ).render(); // вызываем класс MenuCard и передаем в него аргументы
 });
