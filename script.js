daba6d26f7ba80425a146c23532513a5afab44af
Dmitry <d.a16@yandex.ru>
2022-05-14 18:34:00 +0300

    Rest operator and default parameters (ES6)

--- script.js
+++ script.js
@@ -1,39 +1,13 @@
 'use strict';
 
-class Rectangle {
-   constructor(height, width) { // height, width получаем из сервера
-      this.height = height;
-      this.width = width;
-   }
+const log = function(a, b, ...rest) { // Rest оператор – собрал отдельные элементы в один массив
+   console.log();
+};
 
-   calcArea() { // метод calcArea для подсчета площади
-      return this.height * this.width;
-   }
+log('basic', 'rest', 'operator', 'usage');
+
+function calcOrDouble(number, basis = 2) { // Параметру по умолчанию (basis) присвоим 2
+   console.log(number * basis);
 }
 
-const square = new Rectangle(10, 10); // в переменной square лежит объект, в котором свойства (height, width) и метод calcArea
-const long = new Rectangle(20, 100); // создадим еще одну фигуру - прямоугольник
-
-console.log(square.calcArea());
-console.log(long.calcArea());
-
-// Принципы объектно-ориентированного программирования:
-// 1.	Абстракция – отделение концепции от ее экземпляра. (шаблон и экземпляры)
-// 2.	Наследование – способность нашего объекта (класса) базироваться на другом объекте (классе).
-
-class ColoredRectangleWithText extends Rectangle { // extends - наследование от класса Rectangle
-   constructor(height, width, text, bgColor) {
-      super(height, width); // super - вызывает суперконструкор родителя (всегда на 1 строке в конструкторе)
-      this.text = text;
-      this.bgColor = bgColor;
-   }
-
-      showMyProps() {
-      console.log(`Текст: ${this.text}, цвет: ${this.bgColor}`);
-   }
-}
-
-const div = new ColoredRectangleWithText(25, 10, 'Hello', 'red');
-
-div.showMyProps();
-console.log(div.calcArea());
+calcOrDouble(3); // второй аргумент задается по умолчанию
