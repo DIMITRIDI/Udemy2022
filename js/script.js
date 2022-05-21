f2fa8bdfa3e2d9b45f00d774cc90ae060330b5e8
Dmitry <d.a16@yandex.ru>
2022-05-21 21:00:58 +0300

    Creating a calculator on the site, part 1

--- js/script.js
+++ js/script.js
@@ -523,5 +523,73 @@ window.addEventListener('DOMContentLoaded', () => {
       });
    });
 
+   // Calculator
 
+   const result = document.querySelector('.calculating__result span');
+   let sex = 'female', // устанавливается по дефолту female
+      height, weight, age,
+      ratio = 1.375; // устанавливается по дефолту 1.375
+
+   function calcTotal() { // основная функция для подсчета каллорий
+      if (!sex || !height || !weight || !age || !ratio) { // функция отрабатывает только при заполнении всех данных
+         result.textContent = '____'; // Можете придумать что угодно            
+         return;
+      }
+      if (sex === 'female') {
+         result.textContent = Math.round((447.6 + (9.2 * weight) + (3.1 * height) - (4.3 * age)) * ratio);
+      } else {
+         result.textContent = Math.round((88.36 + (13.4 * weight) + (4.8 * height) - (5.7 * age)) * ratio);
+      }
+   }
+
+   calcTotal();
+
+   function getStaticInformation(parentSelector, activeClass) {
+      const elements = document.querySelectorAll(`${parentSelector} div`); // внутри родителя получаем все div
+
+      elements.forEach(elem => {
+         elem.addEventListener('click', (e) => { // отслеживаем клики по родительскому элементу
+            if (e.target.getAttribute('data-ratio')) { // если у блока есть data-атрибут, то меняем data-ratio
+               ratio = +e.target.getAttribute('data-ratio');
+            } else { // или если у блока есть id, то меняем его
+               sex = e.target.getAttribute('id');
+            }
+
+            elements.forEach(elem => { // сначала activeClass убираем у всех элементов
+               elem.classList.remove(activeClass);
+            });
+
+            e.target.classList.add(activeClass); // и добавляем activeClass елементу на котором произошло событие
+
+            calcTotal(); // функция запускается каждый раз при каком-либо изменении пользователем данных
+         });
+      });
+   }
+
+   getStaticInformation('#gender', 'calculating__choose-item_active');
+   getStaticInformation('.calculating__choose_big', 'calculating__choose-item_active');
+
+   function getDynamicInformation(selector) {
+      const input = document.querySelector(selector);
+
+      input.addEventListener('input', () => {
+         switch(input.getAttribute('id')) { // при помощи swith case проверяем в какой input кликнул пользователь
+            case "height":
+               height = +input.value;
+               break;
+            case "weight":
+               weight = +input.value;
+               break;
+            case "age":
+               age = +input.value;
+               break;
+         }
+
+         calcTotal(); // функция запускается каждый раз при каком-либо изменении пользователем данных
+      });
+   }
+
+   getDynamicInformation('#height');
+   getDynamicInformation('#weight');
+   getDynamicInformation('#age');
 });
