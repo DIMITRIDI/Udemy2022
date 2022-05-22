26ceb64c2ec3b74d96d29f5db63010cdde26b381
Dmitry <d.a16@yandex.ru>
2022-05-22 17:34:54 +0300

    Encapsulation

--- script.js
+++ script.js
@@ -1,17 +1,62 @@
 'use strict';
 
-const persone = {
-   name: 'Alex',
-   age: 25,
+// function User (name, age) { // функция-конструктор
+//    this.name = name;
+//    let userAge = age; // принимает параметр, который заложен при создании объекта
 
-   get userAge() {
-      return this.age;
-   },
+//    this.say = function () {
+//       console.log(`Имя пользователя: ${this.name}, возраст ${userAge}`);
+//    };
 
-   set userAge(num) {
-      this.age = num;
+//    this.getAge = function() {
+//       return userAge;
+//    };
+
+//    this.setAge = function(age) { // внутри возможны любые манипуляции с userAge
+//       if (typeof age === 'number' && age > 0 && age < 199) {
+//          userAge = age;
+//       } else {
+//          console.log('Недопустимое значение');
+//       }
+//    };
+// }
+
+// const ivan = new User('Ivan', 27);
+// console.log(ivan.name);
+// console.log(ivan.getAge()); // получим возраст
+
+// ivan.setAge(30); // изменим возраст
+// ivan.setAge(300); // в консоли получим недопустимое значение
+
+// console.log(ivan.getAge()); // значение переменной возраст в текущий момент
+
+// ivan.say(); // возраст отображается и присвоено значение 30
+
+class User {
+   constructor(name, age) {
+      this.name = name;
+      this._age = age; // принимает параметр, который заложен при создании объекта
    }
-};
 
-console.log(persone.userAge = 30);
-console.log(persone.userAge);
+   #surname = 'Petrychenko';
+
+   say = () => {
+      console.log(`Имя пользователя: ${this.name}${this.#surname}, возраст ${this._age}`);
+   }
+
+   get age() {
+      return this._age;
+   }
+
+   set age(age) { // внутри возможны любые манипуляции с userAge
+      if (typeof age === 'number' && age > 0 && age < 199) {
+         this._age = age;
+      } else {
+         console.log('Недопустимое значение');
+      }
+   }
+}
+
+const ivan = new User('Ivan', 27);
+console.log(ivan.surname);
+ivan.say();
