4c817cb853d74717edcd38bab1ef55db6f374103
Dmitry <d.a16@yandex.ru>
2022-05-23 10:37:47 +0300

    Reception module, how and why to use it

--- script.js
+++ script.js
@@ -1,62 +1,22 @@
 'use strict';
 
-// function User (name, age) { // функция-конструктор
-//    this.name = name;
-//    let userAge = age; // принимает параметр, который заложен при создании объекта
+const number = 1;
+(function (){ // анонимная самовызывающаяся функция
+   let number = 2;
+   console.log(number);
+   console.log(number + 3);
+}());
 
-//    this.say = function () {
-//       console.log(`Имя пользователя: ${this.name}, возраст ${userAge}`);
-//    };
+console.log(number);
 
-//    this.getAge = function() {
-//       return userAge;
-//    };
+const user = (function (){
+   const privat = function () {
+      console.log('I am privat!');
+   };
 
-//    this.setAge = function(age) { // внутри возможны любые манипуляции с userAge
-//       if (typeof age === 'number' && age > 0 && age < 199) {
-//          userAge = age;
-//       } else {
-//          console.log('Недопустимое значение');
-//       }
-//    };
-// }
+   return {
+      sayHello: privat
+   };
+}());
 
-// const ivan = new User('Ivan', 27);
-// console.log(ivan.name);
-// console.log(ivan.getAge()); // получим возраст
-
-// ivan.setAge(30); // изменим возраст
-// ivan.setAge(300); // в консоли получим недопустимое значение
-
-// console.log(ivan.getAge()); // значение переменной возраст в текущий момент
-
-// ivan.say(); // возраст отображается и присвоено значение 30
-
-class User {
-   constructor(name, age) {
-      this.name = name;
-      this._age = age; // принимает параметр, который заложен при создании объекта
-   }
-
-   #surname = 'Petrychenko';
-
-   say = () => {
-      console.log(`Имя пользователя: ${this.name}${this.#surname}, возраст ${this._age}`);
-   }
-
-   get age() {
-      return this._age;
-   }
-
-   set age(age) { // внутри возможны любые манипуляции с userAge
-      if (typeof age === 'number' && age > 0 && age < 199) {
-         this._age = age;
-      } else {
-         console.log('Недопустимое значение');
-      }
-   }
-}
-
-const ivan = new User('Ivan', 27);
-console.log(ivan.surname);
-ivan.say();
+user.sayHello(); // можем экспортировать методы из объектного интерфейса
