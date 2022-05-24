0fd9c6e0e7166f9621b465f00375478a9bc9db6e
Dmitry <d.a16@yandex.ru>
2022-05-24 15:58:44 +0300

    Making your own mistakes

--- script.js
+++ script.js
@@ -1,15 +1,41 @@
 'use strict';
 
+const data = [
+    {
+        id: 'box',
+        tag: 'div'
+    },
+    {
+        id: 'fff',
+        tag: 'nav'
+    },
+    {
+        id: 'circle',
+        tag: 'span'
+    }
+];
+
+// data.forEach((blockObj, i) => {
+//     const block = document.createElement(blockObj.tag);
+
+//     if (!blockObj.id) throw new Error(`В данных под номером ${i + 1} нет id`);
+//     block.setAttribute('id', blockObj.id);
+//     document.body.append(block);
+// });
+
 try {
-   console.log('Normal'); // то действие, которое хотим выполнить
-   console.log(a);
-   console.log('result');
-} catch(error) {
-   console.log(error.name);
-   console.log(error.message);
-   console.log(error.stack);
-} finally {
-   
+   data.forEach((blockObj, i) => {
+      const block = document.createElement(blockObj.tag);
+
+      if (!blockObj.id) throw new SyntaxError(`В данных под номером ${i + 1} нет id`);
+      block.setAttribute('id', blockObj.id);
+      document.body.append(block);
+   });
+}catch(e) {
+   if (e.name === 'SyntaxError') {
+      console.log(e.message);
+   } else throw e;
 }
 
-console.log('Still normal');
+// const err = new SyntaxError('sfdgfsag');
+// console.log(err.name, err.message, err.stack);
