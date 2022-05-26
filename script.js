e7ba61575a1b05d039d15f99f813a9bcfd59262c
Dmitry <d.a16@yandex.ru>
2022-05-26 12:22:34 +0300

    Web Animations API

--- script.js
+++ script.js
@@ -1,33 +1,28 @@
-const btn = document.querySelector('.btn'),
-      elem = document.querySelector('.box');
-let pos = 0;
+'use strict';
 
-// function myAnimation() {
-//    let pos = 0;
-
-//    const id = setInterval(frame, 10);
-//    function frame() {
-//       if (pos == 300) {
-//          clearInterval(id);
-//       } else {
-//          pos++;
-//          elem.style.top = pos + "px";
-//          elem.style.left = pos + 'px';
-//       }
-//    }
-// }
-
-function myAnimation() {
-   pos++;
-   elem.style.top = pos + "px";
-   elem.style.left = pos + 'px';
-
-   if (pos < 300) {
-      requestAnimationFrame(myAnimation);
+const btnPhone = document.querySelector('#iphone'),
+      btnMacbook = document.querySelector('#macbook'),
+      images = document.querySelectorAll('img');
+let phoneAnimation;
+btnPhone.addEventListener('click', () => {
+   if (!phoneAnimation) { // если анимации еще не существует, то будем ее создавать
+      phoneAnimation = images[0].animate([
+         {transform: 'translateY(0) rotate(0deg)', 
+         filter: 'opacity(100%)'},
+         {transform: 'translateY(100px) rotate(180deg)', 
+         filter: 'opacity(60%)'},
+         {transform: 'translateY(-100px) rotate(270deg)', 
+         filter: 'opacity(30%)'},
+         {transform: 'translateY(0) rotate(360deg)', 
+         filter: 'opacity(100%)'}
+      ], {
+         duration: 3000,
+         iterations: Infinity
+      });
+   } else if (phoneAnimation.playState === 'paused') {
+      phoneAnimation.play();
+   } else {
+      phoneAnimation.pause();
    }
-}
+});
 
-btn.addEventListener('click', () => requestAnimationFrame(myAnimation)); // запуск функции
-
-let id = requestAnimationFrame(myAnimation);
-cancelAnimationFrame(id); // завершение выполнения анимации
