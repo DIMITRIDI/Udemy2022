b93aceef854c7d4a553670947a78e4648cdf5046
Dmitry <d.a16@yandex.ru>
2022-05-26 16:03:11 +0300

    Event loop, detailed work of asynchronous and synchronous operations

--- script.js
+++ script.js
@@ -1,28 +1,19 @@
 'use strict';
 
-const btnPhone = document.querySelector('#iphone'),
-      btnMacbook = document.querySelector('#macbook'),
-      images = document.querySelectorAll('img');
-let phoneAnimation;
-btnPhone.addEventListener('click', () => {
-   if (!phoneAnimation) { // если анимации еще не существует, то будем ее создавать
-      phoneAnimation = images[0].animate([
-         {transform: 'translateY(0) rotate(0deg)', 
-         filter: 'opacity(100%)'},
-         {transform: 'translateY(100px) rotate(180deg)', 
-         filter: 'opacity(60%)'},
-         {transform: 'translateY(-100px) rotate(270deg)', 
-         filter: 'opacity(30%)'},
-         {transform: 'translateY(0) rotate(360deg)', 
-         filter: 'opacity(100%)'}
-      ], {
-         duration: 3000,
-         iterations: Infinity
-      });
-   } else if (phoneAnimation.playState === 'paused') {
-      phoneAnimation.play();
-   } else {
-      phoneAnimation.pause();
-   }
-});
+console.log(1);
 
+setTimeout(() => {
+   console.log('timeout');
+}, 2000);
+
+setTimeout(() => {
+   console.log('timeout_4000');
+}, 4000);
+
+console.log(2);
+
+setTimeout(() => {
+   console.log(1);
+}, 0);
+
+console.log(2);
